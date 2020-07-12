f3bf43b6fb41630557defe95498d13cb5604d815
Emanuel Villalva <emanuelvillalva@outlook.com.ar>
2020-07-12 19:38:20 -0300

    Agregar archivos de proyecto.

---
+++ Singleton/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Persona
+    {
+        private static Persona instancia = null;
+
+        public string texto;
+
+        protected Persona()
+        {
+            this.texto = "Hola mundo";
+        }
+        public static Persona Instancia
+        {
+            get
+            {
+                if(instancia == null)
+                    instancia = new Persona();
+                return instancia;
+            }
+        }
+
+    }
+}
